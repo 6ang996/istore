dec7d0c4504fe1f3471b3c6f2a2eed877730d9f5
ziguayungui <244753440@qq.com>
2021-12-10 23:22:43 +0800

    支持文件下载和上传

--- luci/luci-app-store/luasrc/controller/store.lua
+++ luci/luci-app-store/luasrc/controller/store.lua
@@ -316,6 +316,36 @@ local function split(str,reps)
     return resultStrList
 end
 
+local function ltn12_popen(command)
+
+	local fdi, fdo = nixio.pipe()
+	local pid = nixio.fork()
+
+	if pid > 0 then
+		fdo:close()
+		local close
+		return function()
+			local buffer = fdi:read(2048)
+			local wpid, stat = nixio.waitpid(pid, "nohang")
+			if not close and wpid and stat == "exited" then
+				close = true
+			end
+
+			if buffer and #buffer > 0 then
+				return buffer
+			elseif close then
+				fdi:close()
+				return nil
+			end
+		end
+	elseif pid == 0 then
+		nixio.dup(fdo, nixio.stdout)
+		fdi:close()
+		fdo:close()
+		nixio.exec("/bin/sh", "-c", command)
+	end
+end
+
 -- call get_support_backup_features
 function get_support_backup_features()
     local jsonc = require "luci.jsonc"
@@ -343,8 +373,18 @@ function light_backup()
         stdout = out,
         stderr = err
     }
-    luci.http.prepare_content("application/json")
-    luci.http.write_json(ret)
+    if code == 0 then
+        local light_backup_cmd  = "tar -c %s | gzip 2>/dev/null"
+        local loght_backup_filelist = "/etc/istore/app.list"
+        local reader = ltn12_popen(light_backup_cmd:format(loght_backup_filelist))
+        luci.http.header('Content-Disposition', 'attachment; filename="light-backup-%s-%s.tar.gz"' % {
+			luci.sys.hostname(), os.date("%Y-%m-%d")})
+		luci.http.prepare_content("application/x-targz")
+		luci.ltn12.pump.all(reader, luci.http.write)
+    else
+        luci.http.prepare_content("application/json")
+        luci.http.write_json(ret)
+    end
 end
 
 -- post local_backup
@@ -371,15 +411,51 @@ end
 
 -- post light_restore
 function light_restore()
+    local fd
+    local path
+    local finished = false
+    local tmpdir = "/tmp/"
+    luci.http.setfilehandler(
+        function(meta, chunk, eof)
+            if not fd then
+                path = tmpdir .. "/" .. meta.file
+                fd = io.open(path, "w")
+            end
+            if chunk then
+                fd:write(chunk)
+            end
+            if eof then
+                fd:close()
+                finished = true
+            end
+        end
+    )
+
     local code, out, err, ret
-    code,out,err = is_exec(myopkg .. " restore")
-    ret = {
-        code = code,
-        stdout = out,
-        stderr = err
-    }
-    luci.http.prepare_content("application/json")
-    luci.http.write_json(ret)
+
+    if finished then
+        is_exec("rm /etc/istore/app.list;tar -xzf " .. path .. " -C /")
+        if nixio.fs.access("/etc/istore/app.list") then
+            code,out,err = is_exec(myopkg .. " restore")
+            ret = {
+                code = code,
+                stdout = out,
+                stderr = err
+            }
+            luci.http.prepare_content("application/json")
+            luci.http.write_json(ret)
+        else
+            local error_ret = {code = 500, msg = "File is error!"}
+            luci.http.prepare_content("application/json")
+            luci.http.write_json(error_ret)
+        end
+        -- remove file
+        is_exec("rm " .. path)
+    else
+        ret = {code = 500, msg = "upload failed!"}
+        luci.http.prepare_content("application/json")
+        luci.http.write_json(ret)
+    end
 end
 
 -- post local_restore
